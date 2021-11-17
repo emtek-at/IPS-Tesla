445162b67a4086b66632fac5e28e01ac6d6186fc
bernhard@emtek.at <IbdbSadW88>
2021-11-17 12:22:23 +0100

    bugfixing

--- IPS-TeslaSplitter/module.php
+++ IPS-TeslaSplitter/module.php
@@ -635,9 +635,10 @@ class TeslaSplitter extends IPSModule
 
     private function logger(string $sender, string $message, int $type=KL_DEBUG){
         $show = $this->ReadPropertyBoolean('ShowDebugMessages') ? 'TR ' : 'FA ';
-        $show .= $type.' '.KL_DEBUG.' ';
+        $show .= $type.' '.KL_DEBUG.'|';
         if($this->ReadPropertyBoolean('ShowDebugMessages') && $type==KL_DEBUG)
             $type = KL_MESSAGE;
+        $show .= $type.' ';
 
         $this->LogMessage($show.$sender.' - '.$message, $type);
     }
